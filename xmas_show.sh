0349e25326bdc747afc9573ea77c7e503488f7e4
i839851 <arthur.perry@sap.com>
2022-12-10 20:58:47 -0600

    update tree

--- xmas_show.sh
+++ xmas_show.sh
@@ -6,7 +6,7 @@ trap "kill 0" EXIT
 while :
 do
     python3 dotstar_circuitpytest.py
-    python3 dotstar_chase.py
+#    python3 dotstar_chase.py
     python3 dotstar_random.py
     python3 dotstar_tree.py
     python3 dotstar_animation_tree.py
